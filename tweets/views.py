44c58a99535ff13cb56dc2cde9fc4eac02ca1b3f
Lelouchyagami <pparth3033@gmail.com>
2020-10-06 21:17:00 +0530

    Added Basic tweet-view logic

--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse,Http404
+from .models import Tweet
 
 # Create your views here.
+def tweet_detail_view(request,tweet_id,*args,**kwargs):
+    try:
+        obj = Tweet.objects.get()
+    except:
+        raise Http404
+    return HttpResponse(f"<h1>hello {tweet_id} - {obj.content}<h1>")
